98d6f4008ff66cf4b92d50dec23eac28615a508b
gyanchandmaurya <gyanchandmaurya@gmail.com>
2025-09-26 16:55:39 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -96,3 +96,23 @@
     
 })(jQuery);
 
+
+//  Our Product Execution Process Start----------
+const items = document.querySelectorAll(".accordion-title");
+
+    items.forEach(item => {
+      item.addEventListener("click", () => {
+        const content = item.nextElementSibling;
+        item.classList.toggle("active");
+        content.classList.toggle("open");
+
+        // Close others (if only one should stay open)
+        document.querySelectorAll(".accordion-title").forEach(el => {
+          if (el !== item) {
+            el.classList.remove("active");
+            el.nextElementSibling.classList.remove("open");
+          }
+        });
+      });
+    });
+    //  Our Product Execution Process End----------
